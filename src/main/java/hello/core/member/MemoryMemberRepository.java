54764ce7c1113a3c54ea45d2dccc5f44f0181ed7
mjlee <minjun0124@gmail.com>
2021-04-23 12:40:31 +0900

    Add : git spring hello-core init / member domain

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 동시성 이슈 -> concurrent hashmap 으로 변경해야함
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        // 오류처리는 나중에...
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
