f58ba08bd0290ade0858a6eb35c6cf2ff2c3bbee
mjlee <minjun0124@gmail.com>
2021-04-26 12:33:03 +0900

    feat: Modify discount policy / Violate OCP, DIP

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,17 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // DIP위반 : 인터페이스 뿐만이 아니라 구체화까지 모두 의존하는 상태
+    // OCP위반 : FixDiscount -> RateDiscount 하는 순간 OrderServiceImpl도 수정
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
